8c72235abab08b549b19d079ff10d98107eccc41
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-02-03 16:44:38 -0500

    minor updates

--- swifr_pkg/SWIFr.py
+++ swifr_pkg/SWIFr.py
@@ -304,20 +304,21 @@ if __name__ == '__main__': # added for debugging
 
     # sdm modified lines
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path2trained',action='store',dest='path2trained',default='test_data/simulations_4_swifr/')
+    # location of the trained gmm model to use
+    parser.add_argument('--path2trained',action='store',dest='path2trained',default='test_data/simulations_4_swifr_2class/')
     # interactive allows the user to enter a value for Fst, XP-EHH, iHS, and DDAF as a one-off test (like classifying
     # one row of data).
     parser.add_argument('--interactive',action='store_true',dest='interactive',default=False)
     #use instead of interactive mode to work on a whole file, this is the file to be classified...new stuff. For example
     # this could be the test_test_block_2classes in the application_example folder
     parser.add_argument('--file',action='store',dest='filename')
-    parser.add_argument('--pi',action='store',nargs='+',default=['0.99899', '0.00001', '0.001' ]) #can use with either mode
+    parser.add_argument('--pi',action='store',nargs='+',default=['0.99999', '0.00001' ]) #can use with either mode
     parser.add_argument('--outfile',action='store',default='') # this is where the new file will be stored
     parser.add_argument('--nb',action='store_true',dest='nb',default=True) #run naive bayes (default is false)
     parser.add_argument('--ode',action='store_true',dest='ode',default=True) #output non-normalized AODE output and inidividual ODE scores (default is false)
     args = parser.parse_args()
 
-    args.filename = 'test_data/simulations_4_swifr_test/test/test' # location of data that is to be classified
+    args.filename = 'test_data/simulations_4_swifr_test_2class/test/test'  # location of data that is to be classified
      
     if not args.interactive and not args.filename:
         print("Error: SWIF(r) must be run either with an input file using --file or in " \
