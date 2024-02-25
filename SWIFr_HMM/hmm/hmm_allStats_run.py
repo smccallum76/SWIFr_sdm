07efb323830d3dea5701b3256639a80f4f5a3ac4
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-02-25 18:30:10 -0500

    Avg and max of all stats plus ROC curves added. Plots too.

--- SWIFr_HMM/hmm/hmm_allStats_run.py
+++ SWIFr_HMM/hmm/hmm_allStats_run.py
@@ -11,9 +11,8 @@ from sklearn.metrics import roc_curve, roc_auc_score
 Path to data
 ---------------------------------------------------------------------------------------------------
 """
-stat = 'ihs_afr_std'  # fst is problematic
 state_count = 3  # 3 states implies neutral, sweep, and link; 2 states implies neutral and sweep
-cut_point = 30000  # set to zero if all data is to be used
+cut_point = 0  # set to zero if all data is to be used
 
 if state_count == 2:
     gmm_path = '../../swifr_pkg/test_data/simulations_4_swifr_2class/'
@@ -31,7 +30,6 @@ Load the GMM params from SWIFr_train
 gmm_params = hmm.hmm_init_params(gmm_path)
 classes = gmm_params['class'].unique()
 stats = gmm_params['stat'].unique()
-# gmm_params = gmm_params[gmm_params['stat'] == stat].reset_index(drop=True)  # limit to one stat for now
 
 """ 
 ---------------------------------------------------------------------------------------------------
@@ -118,10 +116,97 @@ for stat in stats:
     # add the gamma values as probabilities for each stat and class
     temp_cols = []
     for g in range(len(gamma)):
-        temp_cols.append(f'P({stat}_{classes[g]})')
-        data[f'P({stat}_{classes[g]})'] = gamma[g]
+        temp_cols.append(f'P({classes[g]}_{stat})')
+        data[f'P({classes[g]}_{stat})'] = gamma[g]
 
     data_cols = ['idx_key', f'pred_class_{stat}'] + temp_cols
     data_orig = pd.merge(data_orig, data[data_cols], on='idx_key', how='left')
 
+""" 
+---------------------------------------------------------------------------------------------------
+Avg. and Max of probabilities
+---------------------------------------------------------------------------------------------------
+"""
+# drop nans where all stats are null (we can't make any prediction here without imputation)
+# nans will be determined using the 'pred_class_<stat>' columns. The actual stat columns could be used
+cols = data_orig.filter(regex=("pred_class_*")).columns.tolist()
+data_noNans = data_orig.dropna(subset=cols, how='all').reset_index(drop=True)
+
+for c in classes:
+    col_temp = data_noNans.filter(regex=(f"P\({c}_*")).columns.tolist()
+    avg_ = data_noNans[col_temp].mean(axis=1)
+    max_ = data_noNans[col_temp].max(axis=1)
+    data_noNans[f"P({c}_All_avg)"] = avg_
+    data_noNans[f"P({c}_All_max)"] = max_
+
 print('done')
+
+""" 
+---------------------------------------------------------------------------------------------------
+ROC - need to make a function [macro averaging] - using average of all probs
+---------------------------------------------------------------------------------------------------
+"""
+# Unique classes
+colors = ['magenta', 'dodgerblue', 'darkviolet', 'blue']  # enough for four classes
+
+# dummy var for HMM
+y_onehot_test = pd.get_dummies(data_noNans['label_num'], dtype=int)
+# dummy var for SWIFr
+y_onehot_test2 = pd.get_dummies(swfr_classified['label_num'], dtype=int)
+
+# initialize figure
+plt.figure(figsize=(9, 7))
+for i in range(len(classes)):  # HMM ROC Curve Loop
+    fpr, tpr, thresh = roc_curve(y_onehot_test.loc[:, i], data_noNans[f"P({classes[i]}_All_avg)"], pos_label=1)
+    auc = roc_auc_score(y_onehot_test.loc[:, i], data_noNans[f"P({classes[i]}_All_avg)"])
+    plt.plot(fpr, tpr, color=colors[i], label=f'HMM {classes[i]} vs Rest (AUC) = ' + str(round(auc, 2)))
+
+for i in range(len(classes)):  # SWIFr ROC Curve Loop
+    swfr_name = 'P(' + classes[i] + ')'
+    fpr, tpr, thresh = roc_curve(y_onehot_test2.loc[:, i], swfr_classified[swfr_name], pos_label=1)
+    auc = roc_auc_score(y_onehot_test2.loc[:, i], swfr_classified[swfr_name])
+    plt.plot(fpr, tpr, linestyle='dashed', color=colors[i], label=f'SWIFr {classes[i]} vs Rest (AUC) = ' + str(round(auc, 2)))
+
+# plot the chance curve
+plt.plot(np.linspace(0, 1, 50 ), np.linspace(0, 1, 50), color='black',
+         linestyle='dashed', label='Chance Level (AUC) = 0.50')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title(f'Macro-Averaging, One-vs-Rest ROC curves [All Stats - AVG]')
+plt.legend()
+plt.show()
+
+""" 
+---------------------------------------------------------------------------------------------------
+ROC - need to make a function [macro averaging] - using max of all probs
+---------------------------------------------------------------------------------------------------
+"""
+# Unique classes
+colors = ['magenta', 'dodgerblue', 'darkviolet', 'blue']  # enough for four classes
+
+# dummy var for HMM
+y_onehot_test = pd.get_dummies(data_noNans['label_num'], dtype=int)
+# dummy var for SWIFr
+y_onehot_test2 = pd.get_dummies(swfr_classified['label_num'], dtype=int)
+
+# initialize figure
+plt.figure(figsize=(9, 7))
+for i in range(len(classes)):  # HMM ROC Curve Loop
+    fpr, tpr, thresh = roc_curve(y_onehot_test.loc[:, i], data_noNans[f"P({classes[i]}_All_max)"], pos_label=1)
+    auc = roc_auc_score(y_onehot_test.loc[:, i], data_noNans[f"P({classes[i]}_All_max)"])
+    plt.plot(fpr, tpr, color=colors[i], label=f'HMM {classes[i]} vs Rest (AUC) = ' + str(round(auc, 2)))
+
+for i in range(len(classes)):  # SWIFr ROC Curve Loop
+    swfr_name = 'P(' + classes[i] + ')'
+    fpr, tpr, thresh = roc_curve(y_onehot_test2.loc[:, i], swfr_classified[swfr_name], pos_label=1)
+    auc = roc_auc_score(y_onehot_test2.loc[:, i], swfr_classified[swfr_name])
+    plt.plot(fpr, tpr, linestyle='dashed', color=colors[i], label=f'SWIFr {classes[i]} vs Rest (AUC) = ' + str(round(auc, 2)))
+
+# plot the chance curve
+plt.plot(np.linspace(0, 1, 50 ), np.linspace(0, 1, 50), color='black',
+         linestyle='dashed', label='Chance Level (AUC) = 0.50')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title(f'Macro-Averaging, One-vs-Rest ROC curves [All Stats - MAX]')
+plt.legend()
+plt.show()
