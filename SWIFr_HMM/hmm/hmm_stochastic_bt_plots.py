674d9751ebd3dd11c70a93ae0a02dcf2728bee07
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-03-28 09:09:30 -0400

    Added sweep like component.

--- SWIFr_HMM/hmm/hmm_stochastic_bt_plots.py
+++ SWIFr_HMM/hmm/hmm_stochastic_bt_plots.py
@@ -22,6 +22,7 @@ ihs['n_neutral'] = np.nansum(ihs[ihs_cols] == 0, axis=1)
 ihs['n_link_left'] = np.nansum(ihs[ihs_cols] == 1, axis=1)
 ihs['n_link_right'] = np.nansum(ihs[ihs_cols] == 2, axis=1)
 ihs['n_sweep'] = np.nansum(ihs[ihs_cols] == 3, axis=1)
+ihs['n_sweepLike'] = np.sum(ihs[['n_link_right', 'n_link_left', 'n_sweep']], axis=1)
 
 ihs_cols2 = [i for i in ihs.columns if 'sb_' not in i]
 ihs_slim = ihs[ihs_cols2]
@@ -34,7 +35,7 @@ Define bins
 bp_per_bin = 50000
 max_bin = np.max(ihs_slim['snp_position'])
 bin_count = int(np.ceil(max_bin/bp_per_bin))
-sweeps = ihs_slim.groupby(pd.cut(ihs_slim['snp_position'], bins=bin_count)).sum()['n_sweep']
+sweeps = ihs_slim.groupby(pd.cut(ihs_slim['snp_position'], bins=bin_count), observed=False).sum()['n_sweepLike']
 
 idx = sweeps.index
 """
