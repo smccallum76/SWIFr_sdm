a476f3c7871930df4b403bcb9ea3357ca125d50f
scottmccallum76 <scott.mccallum.42@gmail.com>
2024-05-27 13:39:04 -0400

    Revised swifr data compilation to include all stats scenario (4 classes).

--- SWIFr_HMM/hmm/hmm_swifr_testCompile.py
+++ SWIFr_HMM/hmm/hmm_swifr_testCompile.py
@@ -14,6 +14,7 @@ gmm_path = '../../swifr_pkg/test_data/thesis_4class/'
 swifr_fst_path = '../../swifr_pkg/test_data/thesis_4class_1stat/class4_fst_test/test/test_classified'
 swifr_xpehh_path = '../../swifr_pkg/test_data/thesis_4class_1stat/class4_xpehh_test/test/test_classified'
 swifr_ihs_path = '../../swifr_pkg/test_data/thesis_4class_1stat/class4_ihs_test/test/test_classified'
+swifr_allStat_path = '../../swifr_pkg/test_data/thesis_4class_test/test/test_classified'
 
 swifr_fst = hmm.hmm_get_data(swifr_fst_path).reset_index(drop=False)
 swifr_fst = swifr_fst.rename(columns={'index': 'idx_key'})
@@ -24,10 +25,14 @@ swifr_xpehh = swifr_xpehh.rename(columns={'index': 'idx_key'})
 swifr_ihs = hmm.hmm_get_data(swifr_ihs_path).reset_index(drop=False)
 swifr_ihs = swifr_ihs.rename(columns={'index': 'idx_key'})
 
+swifr_all = hmm.hmm_get_data(swifr_allStat_path).reset_index(drop=False)
+swifr_all = swifr_all.rename(columns={'index': 'idx_key'})
+
 # save the data
 if update_db == 'yes':
     swifr_fst.to_sql(f"{swifr_table_name}_fst_4class", con=engine, if_exists='replace')
     swifr_xpehh.to_sql(f"{swifr_table_name}_xpehh_4class", con=engine, if_exists='replace')
     swifr_ihs.to_sql(f"{swifr_table_name}_ihs_4class", con=engine, if_exists='replace')
+    swifr_all.to_sql(f"{swifr_table_name}_allStats_4class", con=engine, if_exists='replace')
 
 print('done')
